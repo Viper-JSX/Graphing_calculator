6ce77406f36c4398c43a2bcdcc81bb747ad8f1f9
Yura_viper <yuramcpecs@gmail.com>
2022-04-26 19:51:41 +0200

    Added graphDisplayRange

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ let functionString = "x^2";//x**2;
 let scale = 3; //The bigger the scale the tinier change of x
 let isScaling = false;
 let step = 0.1/scale;
-const range = {l: -10, h: 10};
+
 const originOffset = {x: 0, y: 0};
 const originMovedBy = {x: 0, y: 0};
+const graphFieldInitRange = {x: {l: -10, h: 10}, y: {l: -2, h: 10}};
+const graphRange = {l: -10, h: 10};
+const graphFieldRange = {
+    x: {l: graphFieldInitRange.x.l / scale, h: graphFieldInitRange.x.h},
+    y: {l: graphFieldInitRange.y.l / scale, h: graphFieldInitRange.y.h}
+};
+
 const graphingCalculator = document.getElementById("graphingCalculator");
 const functionInput = document.getElementById("functionInput");
 const scaleInput = document.getElementById("scaleInput");
@@ -25,7 +32,7 @@ graphDisplay.ontouchend = handleScaleEnd;
 */
 
 function handleFunctionInputChnage(event){
-    drawGraph(functionOfX, range);
+    drawGraph(functionOfX, graphRange);
 }
 
 function handleGraphDisplayDragStart(event){
@@ -35,20 +42,16 @@ function handleGraphDisplayDragStart(event){
 }
 
 function handleGraphDisplayDragEnd(event){
-    //show coord of clicked point
     graphDisplay.onmousemove = null;
-    
-    //originOffset.x += (-originMovedBy.x);
-    //originOffset.y += (originMovedBy.y);
-    //originMovedBy.x = 0;
-    //originMovedBy.y = 0;
-    //drawGraph(functionOfX, range);
 }
 
 function handleOriginOffsetChange({ event, dragStartCoords}){
+    if(event.target.classList[0] == "graphPoint"){ //When druring drag mouse on graph
+        return;
+    }
+    
     const dragEndCoords = { x: event.clientX - event.target.offsetLeft, y:event.clientY - event.target.offsetTop /*offetLeft/Top are relatevely to parent (#graphingCalculator)*/};
     const movedBy = {x: dragEndCoords.x - dragStartCoords.x, y: dragEndCoords.y - dragStartCoords.y };
-    console.log(dragStartCoords)
     originMovedBy.x = movedBy.x / scale;
     originMovedBy.y = movedBy.y / scale;
 
@@ -58,16 +61,19 @@ function handleOriginOffsetChange({ event, dragStartCoords}){
     originMovedBy.x = 0;
     originMovedBy.y = 0;
 
-    dragStartCoords.x = dragEndCoords.x;
-    dragStartCoords.y = dragEndCoords.y;
+    dragStartCoords.x = dragEndCoords.x; //Setting coords so that i't a new 
+    dragStartCoords.y = dragEndCoords.y; //refference point
 
-    drawGraph(functionOfX, range);
+    drawGraph(functionOfX, graphRange);
 }
 
 function handleScale(event){
     scale = parseInt(event.target.value);
     step = 0.1 / scale;
-    drawGraph(functionOfX, range);
+    //graphFieldRange.x = {l: graphFieldInitRange.x.l / scale, h: graphFieldInitRange.x.h / scale}, //consider origin desplacement
+    //graphFieldRange.y = {l: graphFieldInitRange.y.l / scale, h: graphFieldInitRange.y.h / scale}; //consider origin desplacement
+    //console.log(graphFieldRange);
+    drawGraph(functionOfX, graphRange);
 }
 
 /* Handle scale
@@ -96,9 +102,9 @@ function functionOfX(x, functionOfX = "x"){
     return (x**2);
 }
 
-function drawGraph(functionOfX, range){
+function drawGraph(functionOfX, graphRange){
     graphDisplay.textContent = "";
-    for(let i = range.l; i <= range.h; i += step){
+    for(let i = graphRange.l; i <= graphRange.h; i += step){
         const newPoint = document.createElement("div");
         newPoint.classList.add("graphPoint");
         x = i/*originOffset.x*/;
@@ -113,4 +119,4 @@ function drawGraph(functionOfX, range){
     }
 }
 
-drawGraph(functionOfX, range);
+drawGraph(functionOfX, graphRange);
